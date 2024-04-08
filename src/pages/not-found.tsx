e3f1361fa7fc6a6569adfa995f6abf29a5b1a15f
polynass <kuuzicheva@gmail.com>
2024-04-08 15:32:44 +0300

    m4-t1

--- src/pages/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import { Link } from 'react-router-dom';
-
-export default function NotFound() {
-  return(
-    <h1>Ошибка 404. Страница не существует. <Link to="/">ВЕРНУТЬСЯ НА ГЛАВНУЮ</Link></h1>
-  );
-}
